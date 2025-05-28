7f113cb772c0cf1635e1286bd3a09dd6fbfa07e4
brahiant <brahiant0@gmail.com>
2025-05-28 07:44:26 -0500

    Componentes de catalogo

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../models/product';
 import { ProductService } from '../services/product.service';
-
+import { CatalogComponent } from './catalog/catalog.component';
 @Component({
   selector: 'app-cart-app',
   standalone: true,
-  imports: [],
+  imports: [CatalogComponent],
   templateUrl: './cart-app.component.html'
 })
 export class CartAppComponent implements OnInit {
