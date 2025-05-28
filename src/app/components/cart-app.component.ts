8f1fa7c9ad15225a4e8721a9aeb9f7db9a39b995
brahiant <brahiant0@gmail.com>
2025-05-28 07:58:47 -0500

    Se agrega el esqueleto de la tabla y las configuraciones para los items del carro de compras, se crea el componente de cart (carrito)

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -2,10 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../models/product';
 import { ProductService } from '../services/product.service';
 import { CatalogComponent } from './catalog/catalog.component';
+import { CartComponent } from './cart/cart.component';
+import { CartItem } from '../models/cartItem';
 @Component({
   selector: 'app-cart-app',
   standalone: true,
-  imports: [CatalogComponent],
+  imports: [CatalogComponent, CartComponent],
   templateUrl: './cart-app.component.html'
 })
 export class CartAppComponent implements OnInit {
@@ -13,6 +15,8 @@ export class CartAppComponent implements OnInit {
   constructor(private productService: ProductService) { }
 
   products: Product[] = [];
+
+  cartItems: CartItem[] = [];
 
   ngOnInit(): void {
     this.products = this.productService.findAll();
